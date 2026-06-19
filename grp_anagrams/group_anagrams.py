124f7713e477a8eaa66a833c4b8c8e0db5e33b11
Tharindu-Deshan <deshantharindu63@gmail.com>
2026-06-19 21:22:06 +0530

    solve group anagram problem

--- grp_anagrams/group_anagrams.py
+++ grp_anagrams/group_anagrams.py
@@ -1,34 +1,29 @@
+# Here this works very effectively when the strings of the inputs are long.
+#if the strings are very short we can use the "".join(sorted(str)) to make the key of anagram dictionary. 
+
+def anagram_key_efficient(str1):
+        alphabet=[0]*26
+        for i in str1:
+            alphabet_position = ord(i)-97
+            alphabet[alphabet_position]+=1
+        alphabet_tuple=tuple(alphabet)
+        return alphabet_tuple
+
+anagram_dict={}
+
 def group_anagrams(strs):
-    def is_anagram(str1,str2):
-        if(len(str1)!=str2):
-            return False
-        similar_chars_in_str1_dict ={}
-        similar_chars_in_str2_dict ={}
-        for char in str1:
-            if(char in similar_chars_in_str1_dict):
-                similar_chars_in_str1_dict[char]+=1
-            else:
-                similar_chars_in_str1_dict[char]=1
-                
-        for char in str2:
-            if(char in similar_chars_in_str2_dict):
-                similar_chars_in_str2_dict[char]+=1
-            else:
-                similar_chars_in_str2_dict[char]=1
-        
-        if(len(similar_chars_in_str2_dict) != len(similar_chars_in_str1_dict)):
-            return False
-
-        for key in similar_chars_in_str1_dict:
-            if(similar_chars_in_str1_dict.get(key,0) != similar_chars_in_str2_dict.get(key,0)):
-                return False
+    for i in strs:
+        sorted_str=anagram_key_efficient(i)
+        if(sorted_str in anagram_dict):
+             anagram_dict[sorted_str].append(i)
         else:
-            return True
+             anagram_dict[sorted_str]=[i]
+     
     
-    anagram_dict = {}
+    return list(anagram_dict.values())
+         
 
-    
-        
-        
-        
 
+
+x=["eat","tea","tan","ate","nat","bat"]
+print(group_anagrams(x))
